768becf24a05ac61c66e3bd181e7093d0efe25e5
ashlialf <ashlialf@student.21-school.ru>
2024-03-03 21:11:06 +0300

    edit mobius.erl

--- mobius.erl
+++ mobius.erl
@@ -1,5 +1,5 @@
 -module(mobius).
--export([is_prime/1, prime_factors/1, find_square_multiples/2]).
+-export([is_prime/1, prime_factors/1, is_square_multiple/1, find_square_multiples/2]).
 
 % Сhecking whether a number is simple
 is_prime(1) -> false;	
