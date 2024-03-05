cc407d77e02ca3c4524ff3662ee0b1f129cc3231
ashlialf <ashlialf@student.21-school.ru>
2024-03-05 16:50:48 +0300

    modify mobius.erl

--- mobius.erl
+++ mobius.erl
@@ -1,20 +1,20 @@
 -module(mobius).
 -export([is_prime/1, prime_factors/1, is_square_multiple/1, find_square_multiples/2]).
 
-% Сhecking whether a number is simple
+% проверка того, является ли число простым
 is_prime(1) -> false;	
 is_prime(N) when N >= 2 -> is_prime(N, 2).
-is_prime(N, Div) when Div * Div > N -> true;    % implementation of the method of sorting divisors
+is_prime(N, Div) when Div * Div > N -> true;    % реализация метода сортировки делителей
 is_prime(N, Div) -> 
 	if
 		N rem Div == 0 -> false;
 		true -> is_prime(N, Div + 1)
 	end.
 
-%Returns a list of prime factors of the number N
+% Возвращает список простых множителей числа N
 prime_factors(N) -> prime_factors(N, 2, []).
 
-% Search for the divisor of the number N in the range from [2, N]
+% Найдите делитель числа N в диапазоне от [2, N]
 prime_factors(N, Div, Result) when Div > N -> Result;
 prime_factors(N, Div, Result) ->
 	case is_prime(Div) of
@@ -30,14 +30,20 @@ prime_factors(N, Div, Result) ->
 		false -> prime_factors(N, Div + 1, Result) 
 	end.
 
-% Argument is divisible by the square of a prime number
+% получает аргумент N, и возвращает true, если аргумент делится на 
+% квадрат простого числа или false, если не длится
+% сортировка списка простых сомножителей, затем поиск повторяющихся сомножителей
 is_square_multiple(N) -> 
 	SortedPrimeFactors = lists:sort(prime_factors(N)),
 	length(SortedPrimeFactors) > sets:size(sets:from_list(SortedPrimeFactors)).
 
+% Если функция находит Count чисел подряд, делящихся на квадрат простого числа 
+% в диапазоне [2, MaxN], она должна вернуть первое число из этой последовательности
 find_square_multiples(Count, MaxN) -> find_square_multiples(Count, MaxN, 2, 2, 0).
-
 find_square_multiples(Count, _, _, First, SeqLen) when SeqLen == Count -> First;
+
+% Если функция не находит серию из Count чисел делящихся на квадрат простого числа 
+% в этом диапазоне, она должна вернуть атом fail
 find_square_multiples(_, MaxN, Current, _, _) when Current > MaxN -> fail;
 find_square_multiples(Count, MaxN, Current, First, SeqLen) ->
 	case is_square_multiple(Current) of
